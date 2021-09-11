c2a5e37a5829444b92f8adc5eec56017b683f8ae
Msmiles <muhammadirfan123@hotmail.com>
2021-09-11 10:31:03 +0000

    added api to views.py withe data, registered report app in settings.py

--- report/views.py
+++ report/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+import json
+import urllib.request
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=d92576bc5d9f70f43271a99cc6d01269').read()
+        json_data = json.loads(res)
+        data = {
+            "country_code": str(json_data['sys']['']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'k',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        data = {}
+    return render(request, 'index.html', data)
