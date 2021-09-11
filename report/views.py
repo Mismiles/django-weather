b606e64a1e600b8f10dd5c7738f760bd765de3be
Msmiles <muhammadirfan123@hotmail.com>
2021-09-11 10:42:45 +0000

    converted api to json format and organised data in view.py file

--- report/views.py
+++ report/views.py
@@ -7,15 +7,16 @@ import urllib.request
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q'+city+'&appid=d92576bc5d9f70f43271a99cc6d01269').read()
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=d92576bc5d9f70f43271a99cc6d01269').read()
         json_data = json.loads(res)
         data = {
-            "country_code": str(json_data['sys']['']),
+            "country_code": str(json_data['sys']['country']),
             "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
             "temp": str(json_data['main']['temp'])+'k',
             "pressure": str(json_data['main']['pressure']),
             "humidity": str(json_data['main']['humidity']),
         }
     else:
+        city = ''
         data = {}
-    return render(request, 'index.html', data)
+    return render(request, 'index.html', {'city': city, 'data': data})
